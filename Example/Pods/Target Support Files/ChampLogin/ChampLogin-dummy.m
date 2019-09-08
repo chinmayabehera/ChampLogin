7fba503fb1767adb58f878c0bb27837eab5bedd5
CUS-32993 <chinmay.behera@foodpanda.in>
2019-09-08 05:45:26 +0530

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/ChampLogin/ChampLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ChampLogin : NSObject
+@end
+@implementation PodsDummy_ChampLogin
+@end
---
+++ Example/Pods/Target Support Files/Pods-ChampLogin_Tests/Pods-ChampLogin_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChampLogin_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ChampLogin_Tests
+@end
